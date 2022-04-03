4593aa231e724994ac44c082ca42a0e7ebb46909
Peter Keller <psilord@cs.wisc.edu>
2022-04-03 00:19:57 -0500

    Started simulating how funinsts work in assembly.

--- asm-tinker/main.c
+++ asm-tinker/main.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int test_fun_inst_0(void);
+typedef unsigned long long u64;
+
+#define U64(x) ((u64)x)
+#define TAGVAL_FUNINST_PRED		0b1101
+#define TAGVAL_FUNINST_FUN		0b1101
+#define TAGVAL_FUNINST_CLOSURE	0b1111
+#define TAG_FUNINST_FUN(v) ((u64)(((u64)v) | TAGVAL_FUNINST_FUN))
+#define TAG_FUNINST_CLOSURE(v) ((u64)(((u64)v) | TAGVAL_FUNINST_CLOSURE))
+#define IS_FUNINST(v) ((u64)(((u64)v) & TAGVAL_FUNINST_PRED))
+
+extern u64 tinker_0(u64 v0, u64 v1);
+
+extern u64 funinst_fun_0(void);
+extern u64 funinst_fun_1(u64 v0);
+
+extern u64 invoke_funinst_0(u64 funinst);
+extern u64 invoke_funinst_1(u64 funinst, u64 v0);
+
+void emit(char *msg, u64 val) 
+{
+	printf("%s -> 0x%08llX\n", msg, val);
+}
 
 int main(void)
 {
-	unsigned long long val;
+	u64 val, fi;
 
-	val = test_fun_inst_0();
+	val = tinker_0(U64(0x10), U64(0x20));
+	emit("tinker_0()", val);
 
-	printf("test_fun_inst_0() returned: 0x%08llX\n", val);
+	printf("\n");
+
+	emit("Real funinst_fun_0", (u64)funinst_fun_0);
+	fi = TAG_FUNINST_FUN(funinst_fun_0);
+	emit("Tagged funinst_fun_0", fi);
+	val = invoke_funinst_0(fi);
+	emit("funinst_fun_0()", val);
+
+	printf("\n");
+
+	emit("Real funinst_fun_1", (u64)funinst_fun_1);
+	fi = TAG_FUNINST_FUN(funinst_fun_1);
+	emit("Tagged funinst_fun_1", fi);
+	val = invoke_funinst_1(fi, (u64)0x10);
+	emit("funinst_fun_1()", val);
+
 
 	return 0;
 }
